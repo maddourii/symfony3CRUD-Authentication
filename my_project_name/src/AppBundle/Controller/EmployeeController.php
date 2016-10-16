b1848a3e04582eb50e1fbd80618292fbdab3f2a8
Mohamed Maddouri <mohamed.maddouri@esprit.tn>
2016-10-16 13:56:07 +0100

    commit

---
+++ my_project_name/src/AppBundle/Controller/EmployeeController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace AppBundle\Controller;
+use AppBundle\Entity\Contact;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class EmployeeController extends Controller
+{
+    /**
+     * @Route("/employee", name="homepage")
+     */
+    public function listAction()
+    {
+        $contacts = $this->getDoctrine()
+                ->getRepository('AppBundle:Contact')
+                ->findAll();
+        return $this->render('employee/index.html.twig', array(
+            'contacts'=>$contacts
+        ));
+    }
+    /**
+     * @Route("/employee/create", name="create")
+     */
+    public function createAction(Request $request)
+    {
+        $contact = new Contact;
+        $form = $this->createFormBuilder($contact)
+                ->add('name', TextType::class,array('attr' => array('class' => 'form-control','style' =>'margin-bottom:15px')))
+                ->add('lname',TextType::class,array('attr'=> array('class' =>'form-control','style' =>'margin-bottom:15px')))
+                ->add('email',EmailType::class,array('attr'=> array('class' =>'form-control','style' =>'margin-bottom:15px')))
+                ->add('Add',SubmitType::class,array('label'=> 'Create Contact','attr'=> array('class' =>'btn btn primary','style' =>'margin-bottom:15px')))
+
+                ->getForm();
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $name =$form['name']->getData();
+            $lname =$form['lname']->getData();
+            $email =$form['email']->getData();
+            $contact->setName($name);
+            $contact->setLname($lname);
+            $contact->setEmail($email);
+            
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+            
+            $this->addFlash(
+                    'notice',
+                    'Contact Added'
+                    );
+            return $this->redirectToRoute('homepage');
+        }
+        
+        return $this->render('employee/create.html.twig',array('form'=> $form->createView()));
+    }
+    /**
+     * @Route("/employee/edit/{id}", name="edit")
+     */
+    public function editAction($id,Request $request)
+    {
+          $contact = $this->getDoctrine()
+                ->getRepository('AppBundle:Contact')
+                ->find($id);
+           $contact->setName($contact->getName());
+            $contact->setLname($contact->getLname());
+            $contact->setEmail($contact->getEmail());
+                  $form = $this->createFormBuilder($contact)
+                ->add('name',TextType::class,array('attr'=> array('class' =>'form-control','style' =>'margin-bottom:15px')))
+                ->add('lname',TextType::class,array('attr'=> array('class' =>'form-control','style' =>'margin-bottom:15px')))
+                ->add('email',EmailType::class,array('attr'=> array('class' =>'form-control','style' =>'margin-bottom:15px')))
+                ->add('Add',SubmitType::class,array('label'=> 'Update Contact','attr'=> array('class' =>'btn btn primary','style' =>'margin-bottom:15px')))
+
+                ->getForm();
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid()){
+            $name =$form['name']->getData();
+            $lname =$form['lname']->getData();
+            $email =$form['email']->getData();
+            
+             $em=$this->getDoctrine()->getManager();
+             $contact = $em->getRepository('AppBundle:Contact')->find($id);
+            $contact->setName($name);
+            $contact->setLname($lname);
+            $contact->setEmail($email);
+            
+           
+            
+            $em->flush();
+            
+            $this->addFlash(
+                    'notice',
+                    'Contact Updated'
+                    );
+            return $this->redirectToRoute('homepage');
+        }
+           return $this->render('employee/edit.html.twig', array(
+            'contact'=>$contact,
+                   'form' =>$form->createView()
+                   
+        ));
+        
+    }
+    /**
+     * @Route("/employee/details/{id}", name="detail")
+     */
+    public function detailAction($id)
+    {
+        
+        $contact = $this->getDoctrine()
+                ->getRepository('AppBundle:Contact')
+                ->find($id);
+        return $this->render('employee/details.html.twig', array(
+            'contact'=>$contact
+        ));
+        
+    }
+    
+     /**
+     * @Route("/employee/delete/{id}", name="delete")
+     */
+    public function deleteAction($id)
+    {
+        
+        $em=$this->getDoctrine()->getManager();
+             $contact = $em->getRepository('AppBundle:Contact')->find($id);
+             $em->remove($contact);
+             $em->flush();
+    
+            
+            $this->addFlash(
+                    'notice',
+                    'Contact removed'
+                    );
+            return $this->redirectToRoute('homepage');
+}
+}
---
+++ my_project_name/src/AppBundle/Entity/Contact.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contact
+ *
+ * @ORM\Table(name="contact")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lname", type="string", length=255)
+     */
+    private $lname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Contact
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set lname
+     *
+     * @param string $lname
+     *
+     * @return Contact
+     */
+    public function setLname($lname)
+    {
+        $this->lname = $lname;
+
+        return $this;
+    }
+
+    /**
+     * Get lname
+     *
+     * @return string
+     */
+    public function getLname()
+    {
+        return $this->lname;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Contact
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
+
